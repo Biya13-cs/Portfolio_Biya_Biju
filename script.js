b4d2fb63f95928fd060cfbbc6401d868db621345
Biya Biju <biyabiju56@gmail.com>
2025-07-02 22:46:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var typed = new Typed(".typing", {
+    strings: [
+      "Front End Developer",
+      "Web Designer",
+      "Tech Enthusisast",
+      "Author"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
